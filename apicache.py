42434ef605080c59bb30593ee3cd6d814c94caa3
anrosent <Anson_Rosenthal@brown.edu>
2014-11-15 08:58:15 -0500

    docstrings and doctest

--- apicache.py
+++ apicache.py
@@ -1,60 +1,95 @@
-#!/usr/bin/env python3
+'''
+    Module providing persistent memoization via a function decorator
+    `persistent_memoize`. Useful in the development loop for scripts using
+    external APIs to enable testing and iteration without worrying about
+    internet connectivity or overloading the APIs.
+'''
+import pickle
+import os.path
 
-import pickle, os.path
 
-def read_cache(fn):
+def read_cache(filename):
+    '''
+        Deserializes the persistent memoization cache in the given file
+    '''
     try:
-        return  pickle.load(fn)
-    except Exception:
+        return pickle.load(filename)
+    except EOFError:
         return {}
 
+
 class Cache(object):
-    '''  '''
-    def __init__(self, fn):
-        if os.path.exists(fn):
-            self.cachefile = open(fn, 'r+b')
+    '''
+        Persistent key-value store; all writes trigger overwrite of the cache
+        on-disk
+    '''
+    def __init__(self, filename):
+        if os.path.exists(filename):
+            self.cachefile = open(filename, 'r+b')
         else:
-            self.cachefile = open(fn, 'w+b')
+            self.cachefile = open(filename, 'w+b')
         self.cache = read_cache(self.cachefile)
 
     def get(self, k):
+        '''
+            Retrieves value for key k in cache
+        '''
         return self.cache[k]
 
     def contains(self, k):
+        '''
+            Returns True if the cache contains an entry with key k,
+            False otherwise
+        '''
         return k in self.cache
 
-    # add to cache and persist
-    def put(self, k, v):
-        self.cache[k] = v
+    def put(self, k, val):
+        '''
+            Adds the entry (k, v) to the cache, triggering an update of the
+            cache on-disk
+        '''
 
-        # want to overwrite
+        self.cache[k] = val
+
+        # Overwrite entire cache file with current cache contents
         self.cachefile.seek(0)
         pickle.dump(self.cache, self.cachefile, -1)
 
     def close(self):
+        '''
+            Releases file handle on pickled cache file
+        '''
         self.cachefile.close()
 
 
 def persist_memoize(cachefile):
+    '''
+        Decorator wrapping functions with persistent memoization using
+        a pickled dictionary in the given file
 
-    def decorator_func(f):
+        Test:
+            NOTE: running this leaves a file cachefile in cwd
+
+        >>> @persist_memoize("cachefile")
+        ... def tester(x):
+        ...     print('func eval')
+        ...     return 3*x
+        >>> tester(3)
+        func eval
+        9
+        >>> tester(3)
+        9
+    '''
+    # Close over cache and argument function to wrap
+    def decorator_func(func):
         cache = Cache(cachefile)
         def wrapper(*args, **kwargs):
             key = (args, tuple(kwargs.items()))
             if cache.contains(key):
-                print("Cache hit: %s" % str(key))
                 return cache.get(key)
             else:
-                val = f(*args, **kwargs)
+                val = func(*args, **kwargs)
                 cache.put(key, val)
                 return val
         return wrapper
     return decorator_func
-
-if __name__ == '__main__':
-    @persist_memoize("cachefile")
-    def tester(x):
-        return 3*x
-
-    v1 = tester(3)
-
